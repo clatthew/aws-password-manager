9bab433ac2078eaeefe45d49f193e3d182a2d183
clatthew <mattjamesclough@gmail.com>
2024-11-06 13:04:28 +0000

    lint

--- src/password_manager.py
+++ src/password_manager.py
@@ -7,9 +7,11 @@ def main_loop():
         else:
             running = not menu()
 
+
 def menu():
     print("Please specify [e]ntry, [r]etrieval, [d]eletion, [l]isting or e[x]it:")
     response = input(">>> ")
+    print(f"response: {response}")
     match response:
         case "e":
             entry()
@@ -21,24 +23,30 @@ def menu():
             listing()
         case "x":
             return exit()
-        case _:  
+        case _:
             print("Invalid input. ", end="")
+
 
 def entry():
     print("entry")
 
+
 def retrieval():
     print("retrieval")
+
 
 def deletion():
     print("deletion")
 
+
 def listing():
     print("listing")
+
 
 def exit():
     print("Thank you. Goodbye.")
     return False
+
 
 def authentication():
     print("enter password:")
@@ -46,11 +54,14 @@ def authentication():
     if password == "hello":
         return True
 
+
 def check_credentials():
     pass
 
+
 def get_secret_ids():
     pass
+
 
 if __name__ == "__main__":
     main_loop()
