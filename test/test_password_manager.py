c4104e5a0328c7a323e4c209411034e98bc8286a
clatthew <mattjamesclough@gmail.com>
2024-11-06 13:34:52 +0000

    lint

--- test/test_password_manager.py
+++ test/test_password_manager.py
@@ -4,33 +4,18 @@ from unittest.mock import patch
 
 PATCH_PATH = "src.password_manager."
 
+
 class Testmain_loop:
     @mark.it("Calls authentication function when first run")
     def test_1(self):
         with patch(f"{PATCH_PATH}menu", return_value=True):
             with patch(f"{PATCH_PATH}authentication") as mock:
-                mock.return_value=True
+                mock.return_value = True
                 main_loop()
                 mock.assert_called_once()
 
-class Testmenu:
-    @mark.it("Entering input e, r, d, l or x calls the respective function:")
-    @mark.parametrize(
-        "option,func_name",
-        [
-            ("e", "entry"),
-            ("r", "retrieval"),
-            ("d", "deletion"),
-            ("l", "listing"),
-            ("x", "exit"),
-        ],
-    )
-    def test_inputs(self, option, func_name):
-        with patch(f"{PATCH_PATH}input", return_value=option):
-            with patch(f"{PATCH_PATH}{func_name}") as mock:
-                menu()
-                mock.assert_called_once()
 
+class Testmenu:
     @mark.it("Menu function displays correct options message")
     def test_intro_message(self, capfd):
         with patch(f"{PATCH_PATH}input", return_value="x"):
@@ -56,7 +41,22 @@ class Testmenu:
         )
         add_message = "Invalid input. "
         expected = add_message + std_message
-        result = captured.out[
-            len(std_message) + 1 : len(std_message + expected) + 1
-        ]
+        result = captured.out[len(std_message) + 1 : len(std_message + expected) + 1]
         assert result == expected
+
+    @mark.it("Entering input e, r, d, l or x calls the respective function:")
+    @mark.parametrize(
+        "option,func_name",
+        [
+            ("e", "entry"),
+            ("r", "retrieval"),
+            ("d", "deletion"),
+            ("l", "listing"),
+            ("x", "exit"),
+        ],
+    )
+    def test_inputs(self, option, func_name):
+        with patch(f"{PATCH_PATH}input", return_value=option):
+            with patch(f"{PATCH_PATH}{func_name}") as mock:
+                menu()
+                mock.assert_called_once()
